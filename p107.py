9907a24dd86d93cf357db3e4ea56506a2e4ec695
Mohamed0890 <82848350+Mohamed0890@users.noreply.github.com>
2022-01-21 20:01:36 -0500

    Add files via upload

---
+++ p107.py
@@ -0,0 +1,14 @@
+import csv
+import pandas as pd
+import plotly.express as px
+
+df=pd.read_csv("piselmath.csv")
+mean=df.groupby(["student_id","level"], as_index=False)['attempt'].mean()
+
+fig=px.scatter(mean,
+    x="student_id",
+    y="level",
+    size="attempt",
+    color="attempt"
+)
+fig.show()
